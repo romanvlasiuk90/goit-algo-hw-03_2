7249a6ecfdce17d2b212ba303612f6d21838c5f0
Roman Vlasiuk <romanvlasiuk90@gmail.com>
2024-07-14 14:50:15 +0300

    programmed & tested

---
+++ 2-koch_curve.py
@@ -0,0 +1,41 @@
+import turtle
+
+def koch_curve(t, length, depth):
+    if depth == 0:
+        t.forward(length)
+    else:
+        length /= 3.0
+        koch_curve(t, length, depth-1)
+        t.left(60)
+        koch_curve(t, length, depth-1)
+        t.right(120)
+        koch_curve(t, length, depth-1)
+        t.left(60)
+        koch_curve(t, length, depth-1)
+
+def draw_snowflake(t, length, depth):
+    for i in range(3):
+        koch_curve(t, length, depth)
+        t.right(120)
+
+def main():
+    screen = turtle.Screen()
+    screen.title("Кохів Фрактал Сніжинка")
+    
+    t = turtle.Turtle()
+    t.speed(0)
+    
+    recursion_level = int(input("Будь ласка, введіть рівень рекурсії: "))
+    length = 300  # Можете змінити довжину сторони фрактала за потребою
+
+    t.penup()
+    t.goto(-length/2, length/3)
+    t.pendown()
+
+    draw_snowflake(t, length, recursion_level)
+
+    t.hideturtle()
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
